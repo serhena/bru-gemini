c3e2235666d1320b47c8ef3587cf03f001327777
serhena <automacoes.serhena@gmail.com>
2026-04-02 01:27:32 +1030

    feat: Add next-sitemap for SEO
    Integrates next-sitemap to automatically generate sitemaps and robots.txt files. This enhances search engine discoverability for the Bruno Restrings website.

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://brunorestrings.au',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  exclude: ['/404', '/_not-found'],
+}
