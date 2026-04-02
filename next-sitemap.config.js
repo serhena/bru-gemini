a0d331149005fbdfa0d886c6be4de35c4d1ce1c4
serhena <automacoes.serhena@gmail.com>
2026-04-02 13:27:58 +1030

    refactor: Remove unused next-sitemap configuration
    The `next-sitemap` dependency and its configuration file have been removed. This package was not being used in the project, so its removal cleans up the codebase and dependencies.

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://brunorestrings.au',
-  generateRobotsTxt: true,
-  generateIndexSitemap: false,
-  exclude: ['/404', '/_not-found'],
-}
